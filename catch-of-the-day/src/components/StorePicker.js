6fa7409ab54113af67fa7a77d941d826b0995ccf
Oliver Elias <oliver.elias@outlook.com>
2018-10-30 15:33:09 +1100

    Lesson 4

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class StorePicker extends Component {
+  render() {
+    return (
+      // Hello
+      <form action="" className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
